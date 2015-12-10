bdd58e1a6808ebeaa46d10ed40a4519247e1146d
Yann Mahe <yann@datadoghq.com>
2015-12-10 17:03:14 -0500

    [cassandra] YAML configuration template versioning
    * Enhance `DatadogMonitor` resource with a `version` attribute.
    * Use `version` attribute in `cassandra` recipe to version, i.e.
      select the appriopriate YAML configuration file.
      Two versions are currently available
      * `1` (Default): Legacy YAML configuration file, compatible with
        Cassandra < 2.2.
      * `2`: Required for Cassandra > 2.2, use the YAML configuration file
      * introduced with
        https://github.com/DataDog/dd-agent/commit/2df7566d2d1881ec3a2722a0bde1e3a0a99b3d9a
    * Enable `cassandra_aliasing` option to comply with
      https://github.com/DataDog/dd-agent/pull/2035
    
    More information:
    https://github.com/DataDog/dd-agent/pull/2142

--- providers/monitor.rb
+++ providers/monitor.rb
@@ -20,7 +20,8 @@ action :add do
     end
     variables(
       :init_config => new_resource.init_config,
-      :instances   => new_resource.instances
+      :instances => new_resource.instances,
+      :version => new_resource.version
     )
     cookbook new_resource.cookbook
     notifies :restart, 'service[datadog-agent]', :delayed if node['datadog']['agent_start']
